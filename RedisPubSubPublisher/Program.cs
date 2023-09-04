ee90625a13f3908b9257f389a4ded18ba2dd18ea
tufan35 <34610153+tufan35@users.noreply.github.com>
2023-09-04 20:37:26 +0300

    Redis Pusblisg-Subscriber davranıs modeli örneklendirilmiştir.

---
+++ RedisPubSubPublisher/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using StackExchange.Redis;
+
+var connectiob = await ConnectionMultiplexer.ConnectAsync("localhost:6379");
+var subscriber = connectiob.GetSubscriber();
+
+while (true)
+{
+    Console.WriteLine("Mesaj");
+    string mesj = Console.ReadLine();
+    await subscriber.PublishAsync("mychannel", mesj);
+
+}
---
+++ RedisPubSubSubscriber/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using StackExchange.Redis;
+
+var connectiob = await ConnectionMultiplexer.ConnectAsync("localhost:6379");
+var subscriber = connectiob.GetSubscriber();
+
+await subscriber.SubscribeAsync("mychannel", (channel, message) =>
+{
+    Console.WriteLine(message);
+});
+
+Console.Read();
