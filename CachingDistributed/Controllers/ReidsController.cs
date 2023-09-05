fc9459a0c783a9fafd6cad832ec2bbbe9cbc602a
tufan35 <34610153+tufan35@users.noreply.github.com>
2023-09-05 19:42:32 +0300

    Sentinel Mimarisi örneklendirilmesi yapıldı.

---
+++ CachingDistributed/Controllers/ReidsController.cs
@@ -0,0 +1,25 @@
+﻿using CachingDistributed.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CachingDistributed.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReidsController : ControllerBase
+    {
+        [HttpGet("[action]/{key}/{value}")]
+        public async Task<IActionResult> SetValue(string key, string value)
+        {
+            var redis = await RedisService.RedisMasterDb();
+            await redis.StringSetAsync(key, value);
+            return Ok();
+        }
+        [HttpGet("[action]/{key}")]
+        public async Task<IActionResult> GetValue(string key)
+        {
+            var redis = await RedisService.RedisMasterDb();
+            var data = await redis.StringGetAsync(key);
+            return Ok(data.ToString());
+        }
+    }
+}
---
+++ CachingDistributed/Services/RedisService.cs
@@ -0,0 +1,60 @@
+﻿using StackExchange.Redis;
+
+namespace CachingDistributed.Services
+{
+    public class RedisService
+    {
+        /// <summary>
+        /// Redis sentine lredis ver,tabanın master sunucusu düşmesi durumunda baska bir yedek sunucunun otomatik olarak yerine geçmesini   veri hizmetlrinin kesitnsiiz devam etmesini sağlar
+        /// sentinel master sunucunun durumunu izlemek ve değişiklikeri algılamak diğer sentinel sunucularoyla birlikte calısmaktadır
+        /// sentinel mimarisinde birkac sinetinel sunucusuyla calıslması inerilmektedir.  bu sayede kesintisiz sunucular görevini yapabilecektir.  
+        /// </summary>
+
+        static ConfigurationOptions sentinelOps => new()
+        {
+            EndPoints =
+            {
+                {"localhost",6383 },
+                {"localhost",6384 },
+                {"localhost",6385 }
+
+            },
+            CommandMap = CommandMap.Sentinel,
+            AbortOnConnectFail = false
+        };
+        static ConfigurationOptions masterOps => new()
+        {
+            AbortOnConnectFail = false
+        };
+
+
+        public static async Task<IDatabase> RedisMasterDb()
+        {
+            ConnectionMultiplexer sentinelConn = await ConnectionMultiplexer.SentinelConnectAsync(sentinelOps);
+            System.Net.EndPoint masterEndpoitn = null;
+            foreach (var endPoint in sentinelConn.GetEndPoints())
+            {
+
+
+                IServer server = sentinelConn.GetServer(endPoint);
+                if (!server.IsConnected)
+                    continue;
+                masterEndpoitn = await server.SentinelGetMasterAddressByNameAsync("mymaster");
+                break;
+            }
+
+            ///dockerize edilen ipler 
+            var localMasterIp = masterEndpoitn.ToString() switch
+            {
+                "172.18.0.2:6379" => "localhost:6379",
+                "172.18.0.3:6379" => "localhost:6380",
+                "172.18.0.4:6379" => "localhost:6381",
+                "172.18.0.5:6379" => "localhost:6382",
+            };
+
+            ConnectionMultiplexer masterConn = await ConnectionMultiplexer.ConnectAsync(localMasterIp);
+            var database = masterConn.GetDatabase();
+            return database;
+        }
+    }
+}
